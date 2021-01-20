8bfa154c523ad085e1932e1796840a411159e529
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-01-20 10:01:18 -0800

    commit tree ad3fc538fc1d2219a1af6a37eb56788c32580538

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (arr) {
+  return [arr[0], arr[1]];
+};
+
+const returnLastTwoDrivers = function (arr) {
+  return [arr[arr.length - 2], arr[arr.length - 1]];
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int) {
+  return function (fare) {
+    return fare * int;
+  };
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+function selectDifferentDrivers(driversArr, fun) {
+  return fun(driversArr);
+}
